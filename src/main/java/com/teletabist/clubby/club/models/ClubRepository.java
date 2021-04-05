fba3f164f167f41153d60333bf888d2e81460dea
b21827608 <b21827608@cs.hacettepe.edu.tr>
2021-04-05 22:33:09 +0300

    Finished the initial, foundational parts of Club Service

--- src/main/java/com/teletabist/clubby/club/models/ClubRepository.java
+++ src/main/java/com/teletabist/clubby/club/models/ClubRepository.java
@@ -3,5 +3,5 @@ package com.teletabist.clubby.club.models;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ClubRepository extends CrudRepository<Club, Integer>{
-    public Club findDistinctBySlug(Club slug);
+    public Club findDistinctBySlug(String slug);
 }
--- src/main/java/com/teletabist/clubby/club/services/ClubService.java
+++ src/main/java/com/teletabist/clubby/club/services/ClubService.java
@@ -1,16 +1,28 @@
 package com.teletabist.clubby.club.services;
 
-import com.teletabist.clubby.club.daos.ClubDAO;
+
 import com.teletabist.clubby.club.models.Club;
+import com.teletabist.clubby.club.models.ClubRepository;
 
 public class ClubService {
-    private final ClubDAO clubDAO;
+    private final ClubRepository clubRepository;
 
-    public ClubService(ClubDAO clubDAO) {
-        this.clubDAO = clubDAO;
+    public ClubService(ClubRepository clubRepository) {
+        this.clubRepository = clubRepository;
     }
 
+    //TODO: add try/catch block and verification parts
     public Club addClub(Club club) {
-        return clubDAO.createClub(club);
+        club = clubRepository.save(club);
+        return club;
+    }
+
+    public Club getClub(String slug) {
+        return clubRepository.findDistinctBySlug(slug);
+    }
+
+    public Iterable<Club> getAll() {
+        return clubRepository.findAll();
     }
 }
+
