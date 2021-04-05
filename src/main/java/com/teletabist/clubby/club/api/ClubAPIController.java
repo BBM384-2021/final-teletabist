55b9e70411ef99340aa2b5203860e5064ea7abca
b21827608 <b21827608@cs.hacettepe.edu.tr>
2021-04-05 22:25:51 +0300

    Deletion of DAOs which are unnecessary. Re-Creation of Club Model, Service, API, Controller, and Repositry; Filling of Club Model and Club Repositry with updated functionality with some need of extra functionality, and lastly initial start of Club Service

---
+++ src/main/java/com/teletabist/clubby/club/api/ClubAPIController.java
@@ -0,0 +1,5 @@
+package com.teletabist.clubby.club.api;
+
+public class ClubAPIController {
+    
+}
--- src/main/java/com/teletabist/clubby/club/models/Club.java
+++ src/main/java/com/teletabist/clubby/club/models/Club.java
@@ -1,69 +1,135 @@
 package com.teletabist.clubby.club.models;
 
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+
 public class Club {
-    private final int id;
-    private final String slug;
-    private final String name;
-    private final String description;
-    private final String profilePhotoURL;
-    private final String website;
-    private final String location;
-    private final int parentId;
 
-    public Club(int id, String slug, String name, String description, String profilePhotoURL, String website,
-            String location, int parentId) {
-        this.id = id;
-        this.slug = slug;
-        this.name = name;
-        this.description = description;
-        this.profilePhotoURL = profilePhotoURL;
-        this.website = website;
-        this.location = location;
-        this.parentId = parentId;
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(length = 10)
+    private Integer id;
+
+    @Column(unique = true, nullable = false, length = 144)
+    private String slug;
+
+    @Column(nullable = false, length = 255)
+    private String name;
     
-    public Club(int id, Club club) {
-        this.id = id;
-        this.slug = club.slug;
-        this.name = club.name;
-        this.description = club.description;
-        this.profilePhotoURL = club.profilePhotoURL;
-        this.website = club.website;
-        this.location = club.location;
-        this.parentId = club.parentId;
+    @Column(columnDefinition = "TEXT")
+    private String description = "";
+
+    //DEĞİŞEBİLİR
+    @Column(length = 1024)
+    private String profilePhotoURL;
+
+    @Column(length = 256)
+    private String website;
+
+    @Column(columnDefinition = "TEXT")
+    private String location;
+
+    @Column(length = 10)
+    private Integer parentId;
+
+    @Column(nullable = false)
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+
+    @Column(nullable = false)
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    public Integer getId() {
+        return id;
     }
 
-    public int getId() {
-        return id;
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getSlug() {
         return slug;
     }
 
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getDescription() {
         return description;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public String getProfilePhotoURL() {
         return profilePhotoURL;
+    }
+
+    public void setProfilePhotoURL(String profilePhotoURL) {
+        this.profilePhotoURL = profilePhotoURL;
     }
 
     public String getWebsite() {
         return website;
     }
 
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
     public String getLocation() {
         return location;
     }
 
-    public int getParentId() {
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Integer getParentId() {
         return parentId;
     }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+
 
     
     
