0408a471b2b411bbb603eabf3de3343709ece759
iamyigitkoc <osmanyigitkoc@hotmail.com>
2021-04-21 23:06:56 +0300

    User profile tyhme

--- src/main/java/com/teletabist/clubby/user/http/UserController.java
+++ src/main/java/com/teletabist/clubby/user/http/UserController.java
@@ -1,15 +1,18 @@
 package com.teletabist.clubby.user.http;
 
 
+import com.teletabist.clubby.user.models.User;
 import com.teletabist.clubby.user.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-@RequestMapping("user")
+@RequestMapping("users")
 @Controller
 public class UserController {
     private final UserService userService;
@@ -23,5 +26,15 @@ public class UserController {
     public String index(ModelMap map){
         map.put("users", this.userService.getAll());        
         return "user/index";
+    }
+
+    @GetMapping("{username}")
+    public ModelAndView show(@PathVariable String username, ModelMap map){
+        User u = this.userService.getUser(username);
+        if(u == null)
+            return new ModelAndView("404");
+        
+        map.put("user", u);        
+        return new ModelAndView("user/single");
     }
 }
