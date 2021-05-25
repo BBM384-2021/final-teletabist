d00e2f73b0a6737e5bd614333e1134473cbde654
iamyigitkoc <osmanyigitkoc@hotmail.com>
2021-05-26 02:23:57 +0300

    Survey Module v0.5 - Search merge - hotfix

--- src/main/java/com/teletabist/clubby/club/models/Club.java
+++ src/main/java/com/teletabist/clubby/club/models/Club.java
@@ -52,18 +52,8 @@ public class Club {
     private String location;
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     @JsonIgnoreProperties({"subclubs", "parent",  "clubRating", "hibernateLazyInitializer", "handler"})
     @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
-=======
-    @JsonIgnoreProperties({"subclubs", "parent",  "clubRating"})
-    @OneToMany(mappedBy = "parent", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
->>>>>>> ed7ae0c... - Added auto survey generation via filtering
-=======
-    @JsonIgnoreProperties({"subclubs", "parent",  "clubRating", "hibernateLazyInitializer", "handler"})
-    @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
->>>>>>> 8d2685e... 	modified:   src/main/java/com/teletabist/clubby/club/models/Club.java
     private Collection<Club> subclubs;
 
     @JsonIgnoreProperties({"subclubs", "parent", "clubRating", "hibernateLazyInitializer", "handler"})
