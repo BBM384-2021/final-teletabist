000fd8aea5fa08d1bf72c2ead62147d39f0eaa71
iamyigitkoc <osmanyigitkoc@hotmail.com>
2021-05-27 02:45:52 +0300

    admin module

---
+++ resources/js/Components/PageLoader.vue
@@ -0,0 +1,27 @@
+  
+<template>
+  <div class="page-loader" v-if="!isloaded">
+    <div class="cube"></div>
+    <div class="cube"></div>
+    <div class="cube"></div>
+    <div class="cube"></div>
+  </div> 
+</template>
+
+<script>
+  export default {
+    data: () => {
+      return {
+        isloaded: false
+      }
+    },
+    mounted() {
+        
+
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
